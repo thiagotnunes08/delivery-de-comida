411808c8b41cbdb56ff5db3165fbcf218c9fe5b2
thiagotnunes08 <thiagotomaznunes.9908@outlook.com>
2022-09-13 08:39:09 -0300

    firts commit

---
+++ src/main/java/br/com/thiagofood/deliverydecomida/DeliveryDeComidaApplication.java
@@ -0,0 +1,13 @@
+package br.com.thiagofood.deliverydecomida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryDeComidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryDeComidaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/thiagofood/deliverydecomida/DeliveryDeComidaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.thiagofood.deliverydecomida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryDeComidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
